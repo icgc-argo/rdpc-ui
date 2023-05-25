44e2dc39982eb9316971d94536ec1a76ccdd7593
Ciarán Schütte <ciaranschutte@gmail.com>
2023-05-25 08:35:33 -0400

    Feat/#957 repo setup (#1)
    * add env
    
    * prettier
    
    * add husky
    
    * add ts-eslint
    
    * use one eslint
    
    * env var + typing
    
    * add docker
    
    * readme update + copyrights
    
    * Update .prettierrc.json
    
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
