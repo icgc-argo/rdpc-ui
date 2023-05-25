8e771f5a7545f70473212161f0d457ab0c77a90d
Ciarán Schütte <ciaranschutte@gmail.com>
2023-05-25 08:35:33 -0400

    Feat/#957 repo setup (#1)
    * add env
    
    * prettier
    
    * add husky
    
    * add ts-eslint
    
    * use one eslint
    
    * env var + typing
    
    * add docker
    
    * readme update + copyrights
    
    * Update .prettierrc.json
    
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+/*
+ * Copyright (c) 2023 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import Image from 'next/image';
+import styles from './page.module.css';
+import { getAppConfig } from './global/config';
 
 export default function Home() {
+  const { TEST_ENV_VAR } = getAppConfig();
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -27,7 +48,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-
+      <h1>env var {`${TEST_ENV_VAR}`}</h1>
       <div className={styles.center}>
         <Image
           className={styles.logo}
@@ -85,11 +106,9 @@ export default function Home() {
           <h2>
             Deploy <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
-  )
+  );
 }
