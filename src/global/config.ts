118318ce27588505b6d7efb32e5df031c2bec7a7
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2023-06-05 09:54:00 -0400

    move global into src, instead of app folder

--- src/global/config.ts
+++ src/global/config.ts
@@ -20,7 +20,7 @@
 type AppConfig = { TEST_ENV_VAR: string };
 
 export const getAppConfig = (): AppConfig => {
-  return {
-    TEST_ENV_VAR: process.env.NEXT_PUBLIC_TEST,
-  };
+	return {
+		TEST_ENV_VAR: process.env.NEXT_PUBLIC_TEST,
+	};
 };
