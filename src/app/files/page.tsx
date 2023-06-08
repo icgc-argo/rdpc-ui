60794bb6067d91201d9f879fa21c1c2a54017460
Dan DeMaria <ddemaria@oicr.on.ca>
2023-06-08 11:08:55 -0400

    Working Redirect w/ URL Join

---
+++ src/app/files/page.tsx
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2023 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import Image from 'next/image';
+import styles from '../page.module.css';
+import { getAppConfig } from '../global/config';
+
+export default function Home() {
+	const { EGO_API_ROOT, EGO_CLIENT_ID } = getAppConfig();
+
+	return (
+		<main className={styles.main}>
+			<div className={styles.description}>
+				<p>
+					Get started by editing&nbsp;
+					<code className={styles.code}>src/app/page.tsx</code>
+				</p>
+				<div>
+					<a
+						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						By{' '}
+						<Image
+							src="/vercel.svg"
+							alt="Vercel Logo"
+							className={styles.vercelLogo}
+							width={100}
+							height={24}
+							priority
+						/>
+					</a>
+				</div>
+			</div>
+			<h1>Welcome! {EGO_CLIENT_ID}</h1>
+			<a href={`/`}>
+				<button>Logout</button>
+			</a>
+			<div className={styles.center}>
+				<Image
+					className={styles.logo}
+					src="/next.svg"
+					alt="Next.js Logo"
+					width={180}
+					height={37}
+					priority
+				/>
+			</div>
+
+			<div className={styles.grid}>
+				<a
+					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<h2>
+						Docs <span>-&gt;</span>
+					</h2>
+					<p>Find in-depth information about Next.js features and API.</p>
+				</a>
+
+				<a
+					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<h2>
+						Learn <span>-&gt;</span>
+					</h2>
+					<p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+				</a>
+
+				<a
+					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<h2>
+						Templates <span>-&gt;</span>
+					</h2>
+					<p>Explore the Next.js 13 playground.</p>
+				</a>
+
+				<a
+					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					className={styles.card}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<h2>
+						Deploy <span>-&gt;</span>
+					</h2>
+					<p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+				</a>
+			</div>
+		</main>
+	);
+}
