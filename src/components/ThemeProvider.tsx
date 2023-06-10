6fdc1b5876cdd7ae10a7122de419d82a2ee4017a
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2023-06-09 21:01:51 -0400

    add style resets

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -17,8 +17,50 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { defaultTheme, ThemeProvider } from '@/lib/emotion';
+import { css, defaultTheme, ThemeProvider, Global } from '@/lib/emotion';
 
 export default ({ children }: { children: React.ReactNode }) => (
-	<ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+	<>
+		<Global
+			styles={css`
+				*,
+				*::before,
+				*::after {
+					box-sizing: border-box;
+				}
+
+				* {
+					margin: 0;
+				}
+
+				body {
+					line-height: 1.5;
+					-webkit-font-smoothing: antialiased;
+				}
+
+				input,
+				button,
+				textarea,
+				select {
+					font: inherit;
+				}
+
+				p,
+				h1,
+				h2,
+				h3,
+				h4,
+				h5,
+				h6 {
+					overflow-wrap: break-word;
+				}
+
+				#root,
+				#__next {
+					isolation: isolate;
+				}
+			`}
+		/>
+		<ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+	</>
 );
