5609cc0fc803a7330786dcedc103a5fd7a5c3427
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2023-06-13 13:22:24 -0400

    use AppBar component from uikit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -17,43 +17,26 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { AppBar, css, useTheme } from '@icgc-argo/uikit';
+import { AppBar, css } from '@icgc-argo/uikit';
 import Link from 'next/link';
 import Image from 'next/image';
-import argoLogo from '../../public/assets/argo-logo.svg';
+import argoLogo from '../../../public/assets/argo-logo.svg';
 
-const Header = () => {
-	const theme = useTheme();
-	return (
-		<header>
-			<div
-				css={css({
-					position: 'sticky',
-					top: '0px',
-					zIndex: 2,
-					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-					width: '100%',
-					padding: '0 18px',
-					minHeight: '58px',
-					backgroundColor: theme.colors.primary,
-				})}
-			>
-				<div css={css({ height: '30px', width: '208px', position: 'relative' })}>
-					<Link href="/">
-						<Image alt="ICGC ARGO" src={argoLogo} fill />
-					</Link>
-				</div>
-
-				{/** keep this div. header will have more items, will be "right-aligned" */}
-				<div>
-					<h1 style={{ color: 'white' }}>Login Button</h1> {/** replace me with Login button */}
-				</div>
+const Header = () => (
+	<header>
+		<AppBar>
+			<div css={css({ height: '30px', width: '208px', position: 'relative' })}>
+				<Link href="/">
+					<Image alt="ICGC ARGO" src={argoLogo} fill />
+				</Link>
 			</div>
-		</header>
-	);
-};
+
+			{/** keep this div. header will have more items, will be "right-aligned" */}
+			<div>
+				<h1 style={{ color: 'white' }}>Login Button</h1> {/** replace me with Login button */}
+			</div>
+		</AppBar>
+	</header>
+);
 
 export default Header;
