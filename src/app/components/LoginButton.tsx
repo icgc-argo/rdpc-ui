f1fb2f662a6bdaf097ae9101c8ac8360149c767b
Dan DeMaria <ddemaria@oicr.on.ca>
2023-06-14 13:43:34 -0400

    Use URL Join

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,9 +1,10 @@
+import urljoin from 'url-join';
 import { GoogleLogin } from '@icgc-argo/uikit';
 import { getAppConfig } from '@/global/config';
 
 const LoginButton = () => {
 	const { EGO_API_ROOT, EGO_CLIENT_ID } = getAppConfig();
-	const loginUrl = `${EGO_API_ROOT}/api/oauth/login/google?client_id=${EGO_CLIENT_ID}`;
+	const loginUrl = urljoin(EGO_API_ROOT, '/api/oauth/login/google', `?client_id=${EGO_CLIENT_ID}`);
 
 	return <GoogleLogin link={loginUrl} />;
 };
