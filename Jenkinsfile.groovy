a8259afafca8a78d85b1b2b619db324e2386142a
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2023-06-17 12:24:05 -0400

    fix qa autodeployment image tag

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -128,10 +128,10 @@ spec:
                 branch "develop"
             }
             steps {
-                build(job: "/provision/update-app-version", parameters: [
-                    [$class: 'StringParameterValue', name: 'RDPC_ENV', value: 'dev' ],
-                    [$class: 'StringParameterValue', name: 'TARGET_RELEASE', value: 'rdpc-ui'],
-                    [$class: 'StringParameterValue', name: 'NEW_APP_VERSION', value: "${version}-${commit}" ]
+                build(job: '/provision/update-app-version', parameters: [
+                    string(name: 'RDPC_ENV', value: 'dev'),
+                    string(name: 'TARGET_RELEASE', value: 'rdpc-ui'),
+                    string(name: 'NEW_APP_VERSION', value: "${version}-${commit}"),
                 ])
                 // sleep(time:30,unit:"SECONDS")
                 // build(job: "/provision/rdpc-gateway-restart", parameters: [
@@ -145,10 +145,10 @@ spec:
                 branch "main"
             }
             steps {
-                build(job: "/provision/update-app-version", parameters: [
-                    [$class: 'StringParameterValue', name: 'RDPC_ENV', value: 'qa' ],
-                    [$class: 'StringParameterValue', name: 'TARGET_RELEASE', value: 'rdpc-ui'],
-                    [$class: 'StringParameterValue', name: 'NEW_APP_VERSION', value: "${version}-${commit}" ]
+                build(job: '/provision/update-app-version', parameters: [
+                    string(name: 'RDPC_ENV', value: 'qa'),
+                    string(name: 'TARGET_RELEASE', value: 'rdpc-ui'),
+                    string(name: 'NEW_APP_VERSION', value: "${version}"),
                 ])
             }
         }
