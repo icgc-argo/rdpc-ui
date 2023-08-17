5f61a325bc8a25949cb019af0405a72a2fe7ab13
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2023-08-17 14:54:12 -0400

    use higher order func to prevent re init

--- src/app/(post-login)/submission/components/ProgramMenu.tsx
+++ src/app/(post-login)/submission/components/ProgramMenu.tsx
@@ -22,7 +22,7 @@ import { css } from '@/lib/emotion';
 import { MenuItem } from '@icgc-argo/uikit';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 
 export default function ProgramMenu({
 	programs,
@@ -38,6 +38,11 @@ export default function ProgramMenu({
 		? programs
 		: programs.filter(({ shortName }) => shortName.search(new RegExp(searchQuery, 'i')) > -1);
 
+	const setActiveProgram =
+		(index: number): MouseEventHandler =>
+		() =>
+			setActiveProgramIndex(index);
+
 	return (
 		<>
 			<Link
@@ -49,7 +54,7 @@ export default function ProgramMenu({
 				<MenuItem
 					level={2}
 					content="All Programs"
-					onClick={() => setActiveProgramIndex(-1)}
+					onClick={setActiveProgram(-1)}
 					selected={pathname === '/submission'}
 				/>
 			</Link>
@@ -59,7 +64,7 @@ export default function ProgramMenu({
 					level={2}
 					key={program.shortName}
 					content={program.shortName}
-					onClick={() => setActiveProgramIndex(programIndex)}
+					onClick={setActiveProgram(programIndex)}
 					selected={programIndex === activeProgramIndex}
 				>
 					<MenuItem level={3}>{program.shortName}</MenuItem>
