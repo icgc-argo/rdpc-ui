61411fb2792ac05b3f8a240a9efdd7a08bff3737
Ciarán Schütte <ciaranschutte@gmail.com>
2023-08-17 23:35:25 -0400

    Update version to 0.5.0
    Homepage feedback (#68)
    
    * fix footer placement links
    
    * fix high res layout not expanding data callout
    
    * use chevron icon
    
    * Update Footer.tsx
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -19,6 +19,7 @@
 
 import { css, useTheme } from '@/lib/emotion';
 import { Icon, Link } from '@icgc-argo/uikit';
+import { Fragment } from 'react';
 import { Col, Row } from 'react-grid-system';
 import urljoin from 'url-join';
 import { useAppConfigContext } from './ConfigProvider';
@@ -124,7 +125,7 @@ const Footer = () => {
 						`}
 					>
 						{navLinks.map(({ displayName, href, target }, index) => (
-							<div key={index}>
+							<Fragment key={index}>
 								<Link
 									target={target}
 									href={href}
@@ -138,7 +139,7 @@ const Footer = () => {
 								{index !== navLinks.length - 1 && (
 									<Icon width="12px" height="12px" name="slash" fill="grey_1" />
 								)}
-							</div>
+							</Fragment>
 						))}
 					</div>
 				</Col>
