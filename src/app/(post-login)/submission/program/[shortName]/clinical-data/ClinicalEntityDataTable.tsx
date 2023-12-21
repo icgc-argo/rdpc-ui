f9b97e9b328268a97012f38431e979c0f9a98a79
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2023-12-21 06:43:09 -0500

    add mock data to work on table

--- src/app/(post-login)/submission/program/[shortName]/clinical-data/ClinicalEntityDataTable.tsx
+++ src/app/(post-login)/submission/program/[shortName]/clinical-data/ClinicalEntityDataTable.tsx
@@ -279,16 +279,239 @@ const ClinicalEntityDataTable = ({
 		setErrorPageSettings(defaultErrorPageSettings);
 	}, [entityType, useDefaultQuery]);
 
-	const { data: clinicalEntityData, loading } = useGetEntityData(
-		program,
-		entityType,
-		page,
-		pageSize,
-		sort,
-		completionState,
-		donorIds,
-		submitterDonorIds,
-	);
+	// const { data: clinicalEntityData, loading } = useGetEntityData(
+	// 	program,
+	// 	entityType,
+	// 	page,
+	// 	pageSize,
+	// 	sort,
+	// 	completionState,
+	// 	donorIds,
+	// 	submitterDonorIds,
+	// );
+
+	const clinicalEntityData = {
+		clinicalData: {
+			programShortName: 'CIA-IE',
+			clinicalEntities: [
+				{
+					entityName: 'donor',
+					entityFields: [
+						'donor_id',
+						'program_id',
+						'submitter_donor_id',
+						'vital_status',
+						'cause_of_death',
+						'survival_time',
+						'primary_site',
+						'height',
+						'weight',
+						'bmi',
+						'genetic_disorders',
+						'menopause_status',
+						'age_at_menarche',
+						'number_of_pregnancies',
+						'number_of_children',
+						'hrt_type',
+						'hrt_duration',
+						'contraception_type',
+						'contraception_duration',
+						'lost_to_followup_after_clinical_event_id',
+					],
+					totalDocs: 2,
+					records: [
+						[
+							{
+								name: 'donor_id',
+								value: '264723',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'program_id',
+								value: 'CIA-IE',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'updatedAt',
+								value: '2023-12-14T18:58:18.113Z',
+								__typename: 'ClinicalRecordField',
+							},
+						],
+						[
+							{
+								name: 'donor_id',
+								value: '264798',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'program_id',
+								value: 'CIA-IE',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'updatedAt',
+								value: '2023-12-14T18:58:19.139Z',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'submitter_donor_id',
+								value: 'DO-1',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'vital_status',
+								value: 'Deceased',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'cause_of_death',
+								value: 'Unknown',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'survival_time',
+								value: '1',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'primary_site',
+								value: 'Adrenal gland',
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'height',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'weight',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'bmi',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'genetic_disorders',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'menopause_status',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'age_at_menarche',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'number_of_pregnancies',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'number_of_children',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'hrt_type',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'hrt_duration',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'contraception_type',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'contraception_duration',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+							{
+								name: 'lost_to_followup_after_clinical_event_id',
+								value: null,
+								__typename: 'ClinicalRecordField',
+							},
+						],
+					],
+					completionStats: [
+						{
+							coreCompletion: {
+								donor: 0,
+								specimens: 0,
+								primaryDiagnosis: 0,
+								followUps: 0,
+								treatments: 0,
+								__typename: 'CoreCompletionFields',
+							},
+							coreCompletionDate: null,
+							coreCompletionPercentage: 0,
+							overriddenCoreCompletion: [],
+							donorId: 264723,
+							entityData: {
+								specimens: {
+									coreCompletionPercentage: 0,
+									normalSpecimensPercentage: 0,
+									tumourSpecimensPercentage: 0,
+									normalRegistrations: 1,
+									normalSubmissions: 0,
+									tumourRegistrations: 0,
+									tumourSubmissions: 0,
+									__typename: 'SpecimenCoreCompletion',
+								},
+								__typename: 'CompletionEntityData',
+							},
+							__typename: 'CompletionStats',
+						},
+						{
+							coreCompletion: {
+								donor: 1,
+								specimens: 0,
+								primaryDiagnosis: 0,
+								followUps: 0,
+								treatments: 0,
+								__typename: 'CoreCompletionFields',
+							},
+							coreCompletionDate: null,
+							coreCompletionPercentage: 0.2,
+							overriddenCoreCompletion: null,
+							donorId: 264798,
+							entityData: {
+								specimens: {
+									coreCompletionPercentage: 0,
+									normalSpecimensPercentage: 0,
+									tumourSpecimensPercentage: 0,
+									normalRegistrations: 1,
+									normalSubmissions: 0,
+									tumourRegistrations: 0,
+									tumourSubmissions: 0,
+									__typename: 'SpecimenCoreCompletion',
+								},
+								__typename: 'CompletionEntityData',
+							},
+							__typename: 'CompletionStats',
+						},
+					],
+					__typename: 'ClinicalDataEntities',
+				},
+			],
+			clinicalErrors: [],
+			__typename: 'ClinicalData',
+		},
+	};
+
+	const loading = false;
 
 	const { clinicalData } =
 		clinicalEntityData == undefined || loading ? emptyClinicalDataResponse : clinicalEntityData;
