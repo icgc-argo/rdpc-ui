351b8eb4f36f15fa70a6c364d44682a50d30d964
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-02-04 00:53:28 -0500

    fix program list table header style

--- src/app/(post-login)/submission/program-table/Header.tsx
+++ src/app/(post-login)/submission/program-table/Header.tsx
@@ -24,7 +24,7 @@ import { ReactNode } from 'react';
 export const TableHeader = ({ children }: { children: ReactNode }) => (
 	<div
 		css={css`
-			font-size: 1.1rem;
+			font-size: 12px;
 			font-weight: 600;
 			padding: 0;
 			&:not(:last-of-type) {
