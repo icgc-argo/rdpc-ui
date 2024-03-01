b0aa3520506ea72b46c61affa56a6a8b9e1d10ce
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-02-29 22:20:50 -0500

    fix html attribute tag

--- src/app/(post-login)/submission/program/[shortName]/clinical-data/ClinicalEntityDataTable.tsx
+++ src/app/(post-login)/submission/program/[shortName]/clinical-data/ClinicalEntityDataTable.tsx
@@ -192,7 +192,7 @@ export const useGetEntityData = (
 
 const DashIcon = (
 	<svg width="10" height="2" viewBox="0 0 10 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<path d="M1 1H9" stroke="#BABCC2" stroke-width="2" stroke-linecap="round" />
+		<path d="M1 1H9" stroke="#BABCC2" strokeWidth="2" strokeLinecap="round" />
 	</svg>
 );
 
