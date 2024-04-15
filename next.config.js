30f68681729ff3bf41fbcdf94924661a5d0ed6ee
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-04-15 11:57:19 -0400

    disable eslint for build

--- next.config.js
+++ next.config.js
@@ -20,10 +20,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    emotion: true,
-  },
+	reactStrictMode: true,
+	compiler: {
+		emotion: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 };
 
 module.exports = nextConfig;
